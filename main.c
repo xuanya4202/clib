e2ba1c2d0e9a917183354458b6b40b05a68efec6
xuanya4202 <haojinglong4202@163.com>
2017-06-08 09:52:44 +0800

    main.c

--- main.c
+++ main.c
@@ -11,18 +11,40 @@ int init_socket(unsigned int port)
     socketfd = socket(AF_INET, SOCK_STREAM, 0);
     if (-1 == socketfd)
     {
-        printf(" socket faile! \n")
+        printf(" socket error! \n")
         return -1;
     }
 
+    bzero((void *)&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htonl(port);
+    bzero((void*)&(server_addr.sin_zero), 8);
 
-
+    if (bind(socketfd, (struct sockaddr*)&server_addr, sizeof(struct sockaddr_in)) < 0)
+    {
+        printf(" bind error! \n");
+        return -1;
+    }
+    //等待队列为5
+    if(listen(socketfd, 5))
+    {
+        printf(" listen error! \n");
+    }
+    
+    return socketfd;
 }
 int start_service(const char *ip, int port)
 {
+    int ret = 0; 
     epoll_fd = epoll_create(100);
-
-
+    
+    ret = init_socket(port)
+    if (0 >= ret)
+    {
+        printf(" init socket faile! \n");
+        return -1;
+    }
 }
 
 int main(int argc, char *argv[])
